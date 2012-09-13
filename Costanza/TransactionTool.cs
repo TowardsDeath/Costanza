bdd367eccc35aad4a358842002b061fec8d39c71
VersoLaMorte <robert@versolamorte.nl>
2012-09-13 17:13:35 +0200

    Add utility for handling transactions.

---
+++ Costanza/TransactionTool.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Transactions;
+
+namespace Costanza
+{
+    /// <summary>
+    /// Provides common utility methods for Transactions.
+    /// </summary>
+    public static class TransactionTool
+    {
+        /// <summary>
+        /// Creates a new TransactionScope without the harmless defaults. 
+        /// </summary>
+        /// <remarks>
+        /// The TransactionScope's default constructor sets values that are 
+        /// considered harmful. The default Timeout setting can lead to unexpected behaviour, while
+        /// the default IsolationLevel is prone to deadlocks.
+        /// Source: http://blogs.msdn.com/b/dbrowne/archive/2010/06/03/using-new-transactionscope-considered-harmful.aspx
+        /// </remarks>
+        /// <returns>
+        /// An new instance of the <c>TransactionScope</c> class, with IsolationLevel set to 
+        /// ReadCommitted, and Timeout is as large as possible.
+        /// </returns>
+        public static TransactionScope CreateTransactionScope()
+        {
+            var options = new TransactionOptions
+            {
+                IsolationLevel = IsolationLevel.ReadCommitted,
+                Timeout = TimeSpan.MaxValue
+            };
+            return new TransactionScope( TransactionScopeOption.Required, options );
+        }
+    }
+}
