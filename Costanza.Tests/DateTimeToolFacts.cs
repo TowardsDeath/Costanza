35966484b2df3567409e0f0fc27ee76b1794a4b3
VersoLaMorte <robert@versolamorte.nl>
2012-09-13 17:02:35 +0200

    Add utility methods for handling dates.

---
+++ Costanza.Tests/DateTimeToolFacts.cs
@@ -0,0 +1,201 @@
+﻿using System;
+
+using Xunit;
+
+namespace Costanza.Tests
+{
+    public class DateTimeToolFacts
+    {
+        public class IsAtLeastAYearAgoMethod
+        {
+            [Fact]
+            public void CorrectlyDeterminesDateFromMoreThanAYearAgo()
+            {
+                // Arrange
+                var date = DateTimeOffset.UtcNow.AddYears( -1 ).AddDays( -1 );
+                // Act
+                bool returned = DateTimeTool.IsAtLeastAYearAgo( date );
+                // Assert
+                Assert.Equal( true, returned );
+            }
+
+            [Fact]
+            public void CorrectlyDeterminesNow()
+            {
+                // Arrange
+                var now = DateTimeOffset.UtcNow;
+                // Act
+                bool returned = DateTimeTool.IsAtLeastAYearAgo( now );
+                // Assert
+                Assert.Equal( false, returned );
+            }
+        }
+
+        public class IsAtLeastAYearFromNowMethod
+        {
+            [Fact]
+            public void CorrectlyDeterminesDateFromMoreThanAYearFromNow()
+            {
+                // Arrange
+                var date = DateTimeOffset.UtcNow.AddYears( 1 ).AddDays( 1 );
+                // Act
+                bool returned = DateTimeTool.IsAtLeastAYearFromNow( date );
+                // Assert
+                Assert.Equal( true, returned );
+            }
+
+            [Fact]
+            public void CorrectlyDeterminesNow()
+            {
+                // Arrange
+                var now = DateTimeOffset.UtcNow;
+                // Act
+                bool returned = DateTimeTool.IsAtLeastAYearFromNow( now );
+                // Assert
+                Assert.Equal( false, returned );
+            }
+        }
+
+        public class IsOngoingMethod
+        {
+            [Fact]
+            public void CorrectlyDeterminesOngoingRange()
+            {
+                // Arrange
+                var start = DateTimeOffset.UtcNow.AddDays( -1 );
+                var end = DateTimeOffset.UtcNow.AddDays( 1 );
+                // Act
+                bool returned = DateTimeTool.IsOngoing( start, end );
+                // Assert
+                Assert.Equal( true, returned );
+            }
+
+            [Fact]
+            public void CorrectlyDeterminesOutsideRange()
+            {
+                // Arrange
+                var start = DateTimeOffset.UtcNow.AddDays( -2 );
+                var end = DateTimeOffset.UtcNow.AddDays( -1 );
+                // Act
+                bool returned = DateTimeTool.IsOngoing( start, end );
+                // Assert
+                Assert.Equal( false, returned );
+            }
+        }
+
+        public class IsTodayMethod
+        {
+            [Fact]
+            public void ReturnsTrueForToday()
+            {
+                // Arrange
+                var now = DateTimeOffset.UtcNow;
+                // Act
+                bool returned = DateTimeTool.IsToday( now );
+                // Assert
+                Assert.Equal( true, returned );
+            }
+
+            [Fact]
+            public void ReturnsFalseForTomorrow()
+            {
+                // Arrange
+                var tomorrow = DateTimeOffset.UtcNow.AddDays( 1 );
+                // Act
+                bool returned = DateTimeTool.IsToday( tomorrow );
+                // Assert
+                Assert.Equal( false, returned );
+            }
+        }
+
+        public class SpansMultipleDaysMethod
+        {
+            [Fact]
+            public void CorrectlyHandlesAFewHoursApartOnDifferentDays()
+            {
+                // Arrange
+                var start = new DateTimeOffset( new DateTime( 2012, 12, 31, 23, 0, 0 ) ); // 31 December 2012 23:00
+                var end = new DateTimeOffset( new DateTime( 2013, 1, 1, 1, 0, 0 ) ); // 1 January 2013 1:00
+                // Act
+                bool returned = DateTimeTool.SpansMultipleDays( start, end );
+                // Assert
+                Assert.Equal( true, returned );
+            }
+
+            [Fact]
+            public void CorrectlyHandlesAFewHoursApartOnSameDay()
+            {
+                // Arrange
+                var start = new DateTimeOffset( new DateTime( 2012, 8, 8, 17, 0, 0 ) ); // 8 August 17:00
+                var end = new DateTimeOffset( new DateTime( 2012, 8, 8, 23, 0, 0 ) ); // 8 August 23:00
+                // Act
+                bool returned = DateTimeTool.SpansMultipleDays( start, end );
+                // Assert
+                Assert.Equal( false, returned );
+            }
+
+            [Fact]
+            public void CorrectlyHandlesExactlyADayApart()
+            {
+                // Arrange
+                var start = new DateTimeOffset( new DateTime( 2012, 8, 8, 17, 0, 0 ) ); // 8 August 17:00
+                var end = new DateTimeOffset( new DateTime( 2012, 8, 9, 17, 0, 0 ) ); // 9 August 17:00
+                // Act
+                bool returned = DateTimeTool.SpansMultipleDays( start, end );
+                // Assert
+                Assert.Equal( true, returned );
+            }
+
+            [Fact]
+            public void CorrectlyHandlesMoreThanADayApart()
+            {
+                // Arrange
+                var start = new DateTimeOffset( new DateTime( 2012, 8, 8, 17, 0, 0 ) ); // 8 August 17:00
+                var end = new DateTimeOffset( new DateTime( 2012, 8, 9, 18, 0, 0 ) ); // 9 August 18:00
+                // Act
+                bool returned = DateTimeTool.SpansMultipleDays( start, end );
+                // Assert
+                Assert.Equal( true, returned );
+            }
+        }
+
+        public class SpansMultipleMonthsMethod
+        {
+            [Fact]
+            public void CorrectlyHandlesMoreThanAMonthApart()
+            {
+                // Arrange
+                var start = new DateTimeOffset( new DateTime( 2012, 1, 1 ) ); // 1 January 2012
+                var end = new DateTimeOffset( new DateTime( 2012, 3, 1 ) ); // 1 March 2012
+                // Act
+                bool returned = DateTimeTool.SpansMultipleMonths( start, end );
+                // Assert
+                Assert.Equal( true, returned );
+            }
+
+            [Fact]
+            public void CorrectlyHandlesLessThanAMonthApart()
+            {
+                // Arrange
+                var start = new DateTimeOffset( new DateTime( 2012, 1, 1 ) ); // 1 January 2012
+                var end = new DateTimeOffset( new DateTime( 2012, 1, 15 ) ); // 15 January 2012
+                // Act
+                bool returned = DateTimeTool.SpansMultipleMonths( start, end );
+                // Assert
+                Assert.Equal( false, returned );
+            }
+
+            [Fact]
+            public void CorrectlyHandlesLessThanAMonthApartInDifferentYears()
+            {
+                // Arrange
+                var start = new DateTimeOffset( new DateTime( 2012, 12, 31 ) ); // 31 December 2012
+                var end = new DateTimeOffset( new DateTime( 2013, 1, 1 ) ); // 1 January 2013
+                // Act
+                bool returned = DateTimeTool.SpansMultipleMonths( start, end );
+                // Assert
+                Assert.Equal( true, returned );
+            }
+        }
+    }
+}
---
+++ Costanza/DateTimeTool.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Costanza
+{
+    public static class DateTimeTool
+    {
+        public static bool IsAtLeastAYearAgo( DateTimeOffset date )
+        {
+            return Math.Abs( ( DateTimeOffset.UtcNow - date ).Days ) > 365;
+        }
+
+        public static bool IsAtLeastAYearFromNow( DateTimeOffset date )
+        {
+            return IsAtLeastAYearAgo( date );
+        }
+
+        public static bool IsOngoing( DateTimeOffset start, DateTimeOffset end )
+        {
+            var now = DateTimeOffset.UtcNow;
+            return start < now && end > now;
+        }
+
+        public static bool IsToday( DateTimeOffset date )
+        {
+            return date.Date.Equals( DateTimeOffset.UtcNow.Date );
+        }
+
+        public static bool SpansMultipleDays( DateTimeOffset start, DateTimeOffset end )
+        {
+            if( ( end - start ).Hours < 24 )
+            {
+                return start.Day != end.Day;
+            }
+            return ( end - start ).Days >= 1;
+        }
+
+        public static bool SpansMultipleMonths( DateTimeOffset start, DateTimeOffset end )
+        {
+            bool sameMonth = start.Month == end.Month;
+            bool sameYear = start.Year == end.Year;
+            return sameYear ? !sameMonth : !sameMonth && !sameYear;
+        }
+    }
+}
